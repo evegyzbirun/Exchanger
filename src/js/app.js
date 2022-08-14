a3059ee7058794cccdb3766c4e60a383ae7a2382
evegyzbirun <zbirunzbirun@gmail.com>
2022-08-13 21:05:43 -0700

    working on app.js file culculate result from usd and rate

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,12 @@ export default class App {
 
   constructor(usd) {
     this.usd = usd;
-    this.rate = 
+    this.rate = 0;
+    this.result = 0;
+  }
+  // this.usd * this.rate(which we reqest from API)
+  initiateBalance() {
+    this.result = this.usd * this.rate;
+    return this.result;
   }
 }
